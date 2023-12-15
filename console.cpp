37f46ee1cf8621f26831e10caf660f8f66db458a
Marina <rinanashaat64@gmail.com>
2023-12-15 17:27:06 +0200

    summation function

--- console.cpp
+++ console.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
+int sum(int a, int b int c);
 int main()
 {
 	cout << "hello world" << endl;
 	cout << "updated" << endl;
+	cout << sum(2, 4, 6);
+}
+int sum(int a, int b, int c)
+{
+	return a + b + c;
 }
