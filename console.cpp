eb695507489270905e732ead7da28e7840fe96da
Marina64-64 <98766777+Marina64-64@users.noreply.github.com>
2023-12-15 17:43:17 +0200

    sum3 by production (#3)

--- console.cpp
+++ console.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-int sum2(int a, int b int c);
+int sum3(int a, int b int c);
 int main()
 {
 	cout << "hello world" << endl;
 	cout << "updated" << endl;
-	cout << sum2(2, 4, 6);
+	cout << sum3(2, 4, 6);
 }
-int sum2(int a, int b, int c)
+int sum3(int a, int b, int c)
 {
 	return a + b + c;
 }
