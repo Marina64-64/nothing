6caf9648d1271eceb174de27c2584ccb2b7d7877
Marina <rinanashaat64@gmail.com>
2023-12-17 03:26:23 +0200

    YAML

--- console.cpp
+++ console.cpp
@@ -4,7 +4,7 @@ int sum3(int a, int b int c);
 int main()
 {
 	cout << "hello world" << endl;
-	cout << "updated" << endl;
+	cout << "updated " << endl;
 	cout << sum3(2, 4, 6);
 }
 int sum3(int a, int b, int c)
